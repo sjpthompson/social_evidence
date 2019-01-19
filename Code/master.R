497e5bfe16e8f10649a844b6a211922d85ac6ec6
sjpthompson <sjpaulthompson@googlemail.com>
2019-01-19 17:40:00 +0000

    Updates

---
+++ Code/master.R
@@ -0,0 +1,6 @@
+#Runs all the code
+
+setwd("C:/Users/Spencer/Desktop/Social Evidence Base/social_evidence/Code")
+
+source("packages.R")
+
--- Code/packages.R
+++ Code/packages.R
@@ -1,1 +1,5 @@
 #This script installs packages used in the project
+
+library(rmarkdown)
+library(tidyverse)
+library(xlsx)
